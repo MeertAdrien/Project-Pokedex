7233ba220850ac1903f772678958a9e67a4c7f03
Adrien Meert <adrien.meert92@gmail.com>
2025-04-03 14:58:26 +0200

    Condition PokemonCard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,31 @@
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        alt: "Bulbasaur"
+    },
+    {
+        name: "mew",
+    },
+];
 
 function PokemonCard() {
+    const pokemon = pokemonList[1];
     return (
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-                alt="This image represents the Pokémon Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
+            <figcaption>
+                <p>{pokemon.name}</p>
+                {pokemon.imgSrc ? (
+                    <img src={pokemon.imgSrc} alt={pokemon.name} />
+                ) : (
+                    <p>???</p>
+                )}
 
+
+            </figcaption>
         </figure>
     );
 }
+
 export default PokemonCard;
